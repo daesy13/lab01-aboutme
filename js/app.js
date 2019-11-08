5720e6f09ed6312f422e9ec5b11bb38a7a4a16ab
daesy <daesyramos@hotmail.com>
2019-11-08 09:32:19 -0800

    creating function to first 5 Q

--- js/app.js
+++ js/app.js
@@ -1,101 +1,72 @@
 'use strict';
 
-// Teach us more about you! Build out an “About Me” webpage that quizzes the user on fun facts gives the user some basic information about who you are and where you came from.
-
-// * As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
-// [x] Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-// [x] Indicates through an alert if the guess is “too high” or “too low”.
-// [x] It should give the user exactly four opportunities to get the correct answer.
-// [x] After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
-// * As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
-// [x] Add a 7th question that has multiple possible correct answers that are stored in an array.
-// [x] Give the user 6 attempts to guess the correct answer.
-// [x] The guesses will end once the user guesses a correct answer or they run out of attempts.
-// [x] Display all the possible correct answers to the user.
-// [x] Consider using a loop of some sort for this question.
-// * As a user, I would like to know my final score so that I can know how well I did.
-// [x] Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
-// [x] In addition to the user stories stated above, continue to use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
-
-// Stay within scope of what was taught so far in class. Do not write functions within your application. Work with the instructional staff directly if you have questions about this requirement.
-
 var score = 0;
 var name = prompt('What is your name?');
 console.log('User name: ' + name);
 
 alert('Hi ' + name + ', Welcome to my page!');
+alert('let\'s play a guessing game about me. Only Yes or No answers are valid.')
+
+// FUNCTION EXAMPLE USING WHILE - LECTURE EXAMPLE
+
+function yesCorrect (question) {
+    if (question.toUpperCase() === 'YES') {
+        alert("Yes!, you are right.");   
+    }
+    else if (question.toUpperCase() === 'NO'){
+        alert("Sorry, wrong answer try again.");
+    }
+    else {  
+        alert("This answer is not valid");
+    }
+}
+
+function noCorrect (question) {
+    if (question.toUpperCase() === 'YES') {
+        alert("Sorry, wrong answer try again.");
+    }
+    else if (question.toUpperCase() === 'NO'){
+        alert("Yes! you are right.");
+    }
+    else {  
+        alert("This answer is not valid");
+    }
+}
+
 
 // Guess Question 1
-var favColor = prompt(name + " let's play a guessing game about me. Is my favorite color Red? Yes or No:");
+var favColor = prompt(name + " Is my favorite color Red? Yes or No:");
 console.log('my favorite color: ' + favColor);
 
-if (favColor.toUpperCase() === 'YES') {
-    alert("Sorry, " + name + " wrong answer.");
-}
-else if (favColor.toUpperCase() === 'NO'){
-    alert("Yes! " + name + " you are right.");
-}
-else {  
-    alert("This answer is not valid");
-}
+noCorrect(favColor);
+
 
 // Guess Question 2
-var coffee = prompt('Do I like coffee? Yes or No:');
+var coffee = prompt(name + ' Do I like coffee? Yes or No:');
 console.log('coffee: ' + coffee);
 
-if (coffee.toUpperCase() === 'YES') {
-    alert("Sorry, " + name + " wrong answer.");
-}
-else if (coffee.toUpperCase() === 'NO') {
-    alert("Yes! " + name + " you are right.");
-}
-else {  
-    alert("This answer is not valid");
-}
+noCorrect(coffee);
+
 
 // Guess Question 3
-var rollerCoaster = prompt('Do I like roller coasters? Yes or No');
+var rollerCoaster = prompt(name + ' Do I like roller coasters? Yes or No');
 console.log('roller coaster: ' + rollerCoaster);
 
-if (rollerCoaster.toUpperCase() === 'YES') {
-    alert("Sorry, " + name + " wrong answer.");
-}
-else if (rollerCoaster.toUpperCase() === 'NO'){
-    alert("Yes! " + name + " you are right.");
-}
-else {  
-    alert("This answer is not valid");
-}
+noCorrect(rollerCoaster);
 
 // Guess Question 4
-var macUser = prompt('Do I prefer Mac computers? Yes or No');
+var macUser = prompt(name + ' Do I prefer Mac computers? Yes or No');
 console.log('mac user: ' + macUser);
 
-if (macUser.toUpperCase() === 'YES') {
-    alert("Yes! " + name + " you are right.");   
-}
-else if (macUser.toUpperCase() === 'NO'){
-    alert("Sorry, " + name + " wrong answer.");
-}
-else {  
-    alert("This answer is not valid");
-}
+yesCorrect(macUser);
 
 // Guess Question 5
-var windowsUser = prompt('Do I prefer Windows computers? Yes or No');
+var windowsUser = prompt(name + ' Do I prefer Windows computers? Yes or No');
 console.log('windows user: ' + windowsUser);
 
-if (favColor.toUpperCase() === 'YES') {
-    alert("Sorry, " + name + " wrong answer.");
-}
-else if (favColor.toUpperCase() === 'NO'){
-    alert("Yes! " + name + " you are right.");
-}
-else {  
-    alert("This answer is not valid");
-}
+noCorrect(windowsUser);
 
-var number = prompt(" Guess my favorite number");
+var number = prompt(name + " Guess my favorite number");
 console.log( "user guess num is: " + number)
 
 // Guess Question 6
