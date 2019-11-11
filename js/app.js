a05f9aa118b114e1d797fcc50ef674305a3562c8
daesy13 <daesyramos@hotmail.com>
2019-11-10 22:06:36 -0800

    fixed q6

--- js/app.js
+++ js/app.js
@@ -7,66 +7,14 @@ console.log('User name: ' + name);
 alert('Hi ' + name + ', Welcome to my page!');
 alert('let\'s play a guessing game about me. Only Yes or No answers are valid.')
 
-// FUNCTION EXAMPLE USING WHILE - LECTURE EXAMPLE
-// function questionOne(){
-//     var expected = false;
-//     // if they didn't answer waht we wanted, keep trying...
-//     while (!expected) {
-//         var city = prompt('am i from charleston?');
-//         console.log('city: ', city);
-//         var home = city.toLocaleLowerCase();
-//         if (home === 'yes'){
-//             alert('correct');
-//             total++;
-//             expected = true;
-//         }
-//     }
-// }
-// END OF LECTURE WHILE LOOP
-
-// FUNCTION EXAMPLE USING DO WHILE - LECTURE EXAMPLE
-// function questionTwo() {
-//     var expected = false;
-//     do {
-//         var state =  prompt('am i from south carolina');
-//         //console.log('state:' , state);
-//         var region = state.toLowerCase();
-//     }
-// }
-// END OF LECTURE DO WHILE LOOP
-
-// FUNCTION EXAMPLE Q7 - LECTURE EXAMPLE
-// function questionSeven() {
-//     var degree = ['apple', 'strawberry', 'orange', 'grape', 'watermelon'];
-
-//     for (var i = 0; i < 6; i++) {
-//         var answer1 = prompt('what type of degree did you have');
-//         var guess = answer1.toLowerCase();
-
-//         for (var j = 0; j < degree.length; j++){
-//             if (guess === degree[j]) {
-//                 alert('nice job')
-//                 total++;
-//                 i = 6;
-//                 break;
-//             }
-//         }
-//         if (i !==6){
-//             alert('try again')
-//         }
-//     }
-// }
-// END OF LECTURE Q7
-
-
 function yesCorrect (question) {
     if (question.toUpperCase() === 'YES') {
-        alert("Yes!, you are right.");   
+        alert("Yes!, you are right.");
     }
     else if (question.toUpperCase() === 'NO'){
         alert("Sorry, wrong answer try again.");
     }
-    else {  
+    else {
         alert("This answer is not valid");
     }
 }
@@ -78,7 +26,7 @@ function noCorrect (question) {
     else if (question.toUpperCase() === 'NO'){
         alert("Yes! you are right.");
     }
-    else {  
+    else {
         alert("This answer is not valid");
     }
 }
@@ -110,59 +58,39 @@ console.log('mac user: ' + macUser);
 
 yesCorrect(macUser);
 
+
 // Guess Question 5
 var windowsUser = prompt(name + ' Do I prefer Windows computers? Yes or No');
 console.log('windows user: ' + windowsUser);
 
 noCorrect(windowsUser);
 
-var number = prompt(name + ' Guess my favorite number');
-console.log( "user guess num is: " + number)
 
 // Guess Question 6
-
-var favNum = 33;
-var counter = 1;
-var number = parseInt(prompt('Guess my favorite number'));
-function guessFavNumber(guess, favNumber) {
-    while (!33 && counter < 5) {
+function guessFavNumber() {
+    var counter = 1;
+    if (counter === 5) {
+        alert('Sorry you run out of guesses');
+    }
+    while (counter < 5 && number !==33) {
+        var number = parseInt(prompt('Guess my favorite number'));
         if (number > 33) {
-            number = parseInt(prompt('You guessed too high! Guess again'));
+            alert('You guessed too high! Guess again');
             counter++;
         } else if (number < 33) {
-            number = parseInt(prompt('You guessed too low! Guess again'));
+            alert('You guessed too low! Guess again');
             counter++;
         }
     }
     if (number === 33) {
         alert('That is correct! My favorite number is 33!');
         score++;
+        counter = 5;
     }
 }
-guessFavNumber(guess, favNum);
-// while(counter < 5){
-//     number = parseInt(prompt(" Guess my favorite number"));
 
-//     if (number < 33) {
-//         alert('you guessed too low');
-//         counter++;
-//     }
-//     else if (number > 33) {
-//         alert('you guessed too high');
-//         counter++;
-//     }
-//     else if (number === NaN || number === null) {
-//         alert('please enter a real number');
-//         counter++;
-//     }
-//     else if (number == 33) {
-//         alert('Congrats you got it right!!!');
-//         score++;
-//         break;
-//     }
-// }
+guessFavNumber();
 
-alert('Nice try! My favorite number is ' + favNum);
 
 // Guess Question 7
 
@@ -176,7 +104,7 @@ function questionSeven() {
         for (var j = 0; j < userFavFruit.length; j++){
             if (guess === userFavFruit[j]) {
                 alert('Congrats you got it right!!!');
-                score++;    
+                score++;
                 i = 6;
                 break;
             }
@@ -190,6 +118,5 @@ function questionSeven() {
 questionSeven();
 
 alert('My favorite fruits are: apple, strawberry, orange, grape and watermelon');
-
 
 alert('Good Job!... You got ' + score + ' correct answers!');
